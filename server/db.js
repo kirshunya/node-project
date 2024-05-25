16bba990fbf64e056de12d3ef6c2709ad3598b03
Nikita Bakunovich <112703570+kirshunya@users.noreply.github.com>
2024-05-25 03:19:18 +0300

    fix.

--- server/db.js
+++ server/db.js
@@ -1,16 +1,12 @@
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql' // или 'postgres', в зависимости от вашей СУБД
-});
+const { Sequelize } = require("sequelize");
 
-//
-// module.exports = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     dialect: "mysql",
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//   }
-// );
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
