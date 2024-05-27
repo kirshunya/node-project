adab375d9ec97a4e985be4e90e4ab164ce977ff9
Nikita Bakunovich <112703570+kirshunya@users.noreply.github.com>
2024-05-27 22:46:51 +0300

    feat: Local DB.

--- js/modules/config.js
+++ js/modules/config.js
@@ -1,7 +1,7 @@
 export const timeOffsetHours = 3;
 // export const API_URL_PART = "s://app.24loto.com";
-export const API_URL_PART = "s://glacial-journey-80042-91081d6e2248.herokuapp.com";
-// export const API_URL_PART = "://127.0.0.1:5001";
+//export const API_URL_PART = "s://glacial-journey-80042-91081d6e2248.herokuapp.com";
+export const API_URL_PART = "://127.0.0.1:5001";
 //export const API_URL_PART = "://194.87.244.199:5001";
 export const IS_HOSTED_STATIC = true;
 
